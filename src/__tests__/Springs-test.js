2822fb911e91d54ae92cf29f589f3c721222cbdd
Brenton Simpson <appsforartists@google.com>
2017-08-08 11:42:47 -0700

    Add @license header
    Reviewers: skevy
    
    Differential Revision: http://codereview.cc/D3292

--- src/__tests__/Springs-test.js
+++ src/__tests__/Springs-test.js
@@ -1,6 +1,10 @@
-/**
- * @flow
+/** @license
+ *  Copyright 2017 Adam Miskiewicz
+ *
+ *  Use of this source code is governed by a MIT-style license that can be found
+ *  in the LICENSE file or at https://opensource.org/licenses/MIT.
  */
+// @flow
 
 import { Spring } from "../";
 
