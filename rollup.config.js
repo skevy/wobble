22ff5e36a71f2171fe8b13465c105d57ee9e97db
Adam Miskiewicz <adam@exponentjs.com>
2017-08-10 01:57:03 -0700

    Update various configuration/repo setup
    Summary:
    - Runs Flow in CI
    - Saves unit test coverage (for use with some coverage tool)
    - Adds `watch` command
    - Adds `lint-staged` to ensure files are prettiered
    - Makes rollup config longer, but easier to understand (imo)
    
    One key change -- I'm still publishing into the `dist/module` dir...but I'm also including a standalone ES build (but transpiled much less than it was before...no polyfills, helpers). The idea is that you should still be able to get the benefits of dead code elimination using webpack, etc even if you're not transpiling node_modules (which would be required when using the stuff inside dist/module. I think this is the right move.
    
    Test Plan: No test plan
    
    Reviewers: appsforartists
    
    Reviewed By: appsforartists
    
    Differential Revision: http://codereview.cc/D3301

--- rollup.config.js
+++ rollup.config.js
@@ -5,16 +5,73 @@ import babel from "rollup-plugin-babel";
 import uglify from "rollup-plugin-uglify";
 import { minify } from "uglify-es";
 
-export default {
+const base = {
   entry: "src/index.js",
+  plugins: [resolve(), commonjs()]
+};
+
+const browser = Object.assign({}, base, {
   sourceMap: true,
   moduleName: "Wobble",
   plugins: [
-    resolve(),
-    commonjs(),
+    ...base.plugins,
     babel({
-      exclude: "node_modules/**" // only transpile our source code
+      exclude: "node_modules/**", // only transpile our source code
+      babelrc: false,
+      presets: [
+        [
+          "env",
+          {
+            targets: {
+              browsers: ["last 2 versions", "safari >= 7"]
+            },
+            modules: false
+          }
+        ],
+        "stage-0"
+      ],
+      plugins: ["external-helpers", "transform-flow-strip-types"]
     }),
     uglify({}, minify)
   ]
-};
+});
+
+const es = Object.assign({}, base, {
+  format: "es",
+  sourceMap: true,
+  plugins: [
+    ...base.plugins,
+    babel({
+      exclude: "node_modules/**", // only transpile our source code
+      babelrc: false,
+      presets: [
+        [
+          "env",
+          {
+            targets: {
+              node: true
+            },
+            modules: false
+          }
+        ],
+        "stage-0"
+      ],
+      plugins: ["external-helpers", "transform-flow-strip-types"]
+    }),
+    uglify({}, minify)
+  ]
+});
+
+let config;
+switch (process.env.ROLLUP_CONFIG) {
+  case "browser":
+    config = browser;
+    break;
+  case "es":
+    config = es;
+    break;
+  default:
+    throw new Error("Must set ROLLUP_CONFIG");
+}
+
+export default config;
